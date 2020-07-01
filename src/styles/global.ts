f21fd12546ee7bdf118c1440587ab4f26144a0f7
Jimmy Bastos <jimmybastosdev@gmail.com>
2020-07-01 09:50:07 -0300

    Dashboard

---
+++ src/styles/global.ts
@@ -0,0 +1,55 @@
+import { createGlobalStyle } from 'styled-components'
+import appBackground from "../assets/background.svg";
+
+const GlobalStyles = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+
+  html {
+    font-size: 16px;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'Roboto', sans-serif;
+    color: #3A3A3A;
+    font-size: 100%;
+    background: #f0f0f5 url(${appBackground}) no-repeat 70% top;
+  }
+
+  *,
+  *:before,
+  *:after {
+    box-sizing: inherit;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    font-family: inherit;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  #root {
+    max-width: 60rem;
+    margin: 0 auto;
+    padding: 4.5rem 1rem 4.5rem 1rem;
+  }
+
+  input {
+    color: inherit;
+  }
+
+  button,input {
+    font-size: 1rem;
+  }
+
+  button {
+    cursor: pointer;
+    border: 0;
+  }
+`
+
+export default GlobalStyles
